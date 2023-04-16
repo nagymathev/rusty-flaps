c3f8efa72c4f525f32ef2039837df149172df339
nagymathev <nagymathev@gmail.com>
2023-04-16 12:36:09 +0200

    default state is Menu

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ fn main() -> BError{
     let context = BTermBuilder::simple80x50()
         .with_title("Flappy")
         .build()?;
-    main_loop(context, State{})
+    main_loop(context, State::new())
 }
 
 enum GameMode {
@@ -13,11 +13,21 @@ enum GameMode {
     GameOver,
 }
 
-struct State {}
+struct State {
+    mode: GameMode,
+}
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         ctx.cls();
         ctx.print(1, 1, "Hello, Bracket World!");
+    }
+}
+
+impl State {
+    fn new() -> Self {
+        Self {
+            mode: GameMode::Menu,
+        }
     }
 }
