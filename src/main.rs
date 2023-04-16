80a72e773cc58f262a4e28953559f677b19ad743
nagymathev <nagymathev@gmail.com>
2023-04-16 12:32:52 +0200

    window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use bracket_lib::prelude::*;
+
+fn main() -> BError{
+    let context = BTermBuilder::simple80x50()
+        .with_title("Flappy")
+        .build()?;
+    main_loop(context, State{})
+}
+
+enum GameMode {
+    Menu,
+    Playing,
+    GameOver,
+}
+
+struct State {}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print(1, 1, "Hello, Bracket World!");
+    }
 }
